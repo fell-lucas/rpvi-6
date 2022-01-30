4c85ee94c0b93277de6aa875cf9748693b611c09
Lucas Alexandre Fell <lucasafell@gmail.com>
2022-01-30 15:07:04 -0300

    adds unidade concedente

--- frontend/src/components/Input/TextInput/TextInput.tsx
+++ frontend/src/components/Input/TextInput/TextInput.tsx
@@ -6,17 +6,22 @@ type InputProps = {
   name: string;
   label: string;
   placeholder?: string;
+  start?: boolean;
 };
 
 export default class TextInput extends Component<InputProps> {
   render() {
     const p = this.props;
     return (
-      <div className='inputAndLabel'>
+      <div
+        className='inputAndLabel'
+        style={p.start ? { justifyContent: 'flex-start' } : {}}
+      >
         <label htmlFor={p.name}>{p.label} : </label>
         <input
           id={p.name}
           className='textInput'
+          style={p.start ? { width: '100%' } : {}}
           type='text'
           placeholder={p.placeholder ?? p.label}
         />
--- frontend/src/pages/Solicitar/UnidadeConcedente.tsx
+++ frontend/src/pages/Solicitar/UnidadeConcedente.tsx
@@ -24,41 +24,59 @@ export default class UnidadeConcedente extends Component {
               <h2>UNIDADE CONCEDENTE/SETOR DA UNIPAMPA</h2>
             </div>
             <div className='inputGroup'>
-              <TextInput
-                label='Nome'
-                name='estagiarioNome'
-                placeholder='Nome completo'
-              />
-              <TextInput label='Endereço' name='estagiarioEndereco' />
-              <TextInput label='Cidade' name='estagiarioCidade' />
-              <TextInput
-                label='Matrícula Nº'
-                name='estagiarioMatricula'
-                placeholder='xxxxxxxxxxx'
-              />
-              <TextInput label='E-mail' name='estagiarioEmail' />
-              <TextInput label='Bairro' name='estagiarioBairro' />
-              <TextInput label='UF' name='estagiarioUF' />
-              <TextInput label='Curso' name='estagiarioCurso' />
-              <TextInput
-                label='Telefone'
-                name='estagiarioTelefone'
-                placeholder='(55) 9999-9999'
-              />
-              <TextInput label='CEP' name='estagiarioCEP' />
-              <TextInput label='Campus' name='estagiarioCampus' />
-              <TextInput label='Semestre' name='estagiarioSemestre' />
               <div
                 style={{
                   gridColumn: 'span 2 / 3',
                   justifyContent: 'flex-start !important',
                 }}
               >
-                <RadioInput label='Estágio Curricular' name='estagiarioEstagio'>
-                  <option value='obrigatorio'>Obrigatório</option>
-                  <option value='naoObrigatorio'>Não Obrigatório</option>
-                </RadioInput>
+                <TextInput start label='Razão Social' name='ucRazao' />
               </div>
+              <TextInput
+                label='Telefone'
+                name='ucTelefone'
+                placeholder='(55) 9999-9999'
+              />
+              <TextInput label='Endereço' name='ucEndereco' />
+              <TextInput label='Bairro' name='ucBairro' />
+              <TextInput label='CEP' name='ucCEP' />
+              <TextInput label='Cidade' name='ucCidade' />
+              <TextInput label='UF' name='ucUF' />
+              <TextInput label='CNPJ' name='ucCNPJ' />
+              <div
+                style={{
+                  gridColumn: 'span 2 / 3',
+                  justifyContent: 'flex-start !important',
+                }}
+              >
+                <TextInput
+                  start
+                  label='Nome do Supervisor de Estágio'
+                  name='ucSupervisorNome'
+                />
+              </div>
+              <TextInput
+                label='Cargo'
+                name='ucSupervisorCargo'
+                placeholder='Cargo do Supervisor de Estágio'
+              />
+              <div
+                style={{
+                  gridColumn: 'span 2 / 3',
+                  justifyContent: 'flex-start !important',
+                }}
+              >
+                <TextInput
+                  start
+                  label='Nome do Representante Legal'
+                  name='ucRepresentanteNome'
+                />
+              </div>
+              <TextInput
+                label='Cargo'
+                name='ucRepresentanteCargo'
+                placeholder='Cargo do Representante Legal'
+              />
             </div>
             <div className='endButtons'>
               <Button link={EstagiarioRoute} type='filled'>
