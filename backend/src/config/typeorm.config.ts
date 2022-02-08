0965776ee2d63ff5eb5ff8e967354fa895315b09
Lucas Alexandre Fell <lucasafell@gmail.com>
2022-02-07 23:34:05 -0300

    creates vercel backend config and adds .env

---
+++ backend/src/config/typeorm.config.ts
@@ -0,0 +1,13 @@
+/* istanbul ignore file */
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DBNAME,
+  autoLoadEntities: true,
+  synchronize: true,
+};
