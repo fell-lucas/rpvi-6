add00f07c64aec4e231fa186043627042ebcd9fa
Lucas Alexandre Fell <lucasafell@gmail.com>
2022-02-08 00:44:30 -0300

    updates cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: [
       'http://127.0.0.1',
