4272395b4fb5facb41ecfa17dbe3385dc30c7c54
Lucas Alexandre Fell <lucasafell@gmail.com>
2022-02-08 00:16:36 -0300

    updates cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors({
+    origin: [
+      'http://127.0.0.1',
+      'http://localhost',
+      'https://rpvi-6-front.vercel.app/',
+    ],
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
