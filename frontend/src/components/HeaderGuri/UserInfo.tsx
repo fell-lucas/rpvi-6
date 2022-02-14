a5ab4cb2202e044d01f16be6d47b5fd5d84e4a7a
Lucas Alexandre Fell <lucasafell@gmail.com>
2022-02-14 15:37:52 -0300

    updates info

--- frontend/src/components/HeaderGuri/UserInfo.tsx
+++ frontend/src/components/HeaderGuri/UserInfo.tsx
@@ -1,35 +1,51 @@
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
-import { faSignOutAlt, faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames';
 import ContentLoader from 'react-content-loader';
 import Swal from 'sweetalert2';
 
 import useUser from '../../hooks/useUser';
+import { UserRole } from '../../models';
 
 export default function UserInfo() {
   const { user, isUserLoading } = useUser();
+  console.log(user);
 
   return (
-    <div className='flex gap-2 justify-around bg-primary rounded-md p-2 text-white'>
+    <div className='text-ellipsis flex gap-2 justify-between bg-primary rounded-md px-4 p-2 text-white'>
       <div className='flex gap-2 items-center'>
         {isUserLoading ? (
-          <ContentLoader className='w-24 max-h-4' foregroundColor='#d6d6d6'>
-            <rect
-              key={`rect_userInfoText`}
-              x='0'
-              y='0'
-              rx='8'
-              ry='8'
-              width='100%'
-              height='100%'
-            />
-          </ContentLoader>
-        ) : (
           <>
-            <FontAwesomeIcon icon={faUserCircle as IconProp} />
-            {user !== undefined && user.name}
+            <ContentLoader className='w-1/5 max-h-4' foregroundColor='#d6d6d6'>
+              <rect rx='5' ry='5' width='100%' height='100%' />
+            </ContentLoader>
+            <ContentLoader className='w-1/5 max-h-4' foregroundColor='#d6d6d6'>
+              <rect rx='5' ry='5' width='100%' height='100%' />
+            </ContentLoader>
+            <ContentLoader className='w-3/5 max-h-4' foregroundColor='#d6d6d6'>
+              <rect rx='5' ry='5' width='100%' height='100%' />
+            </ContentLoader>
           </>
+        ) : (
+          <div className='flex justify-start gap-2 w-full'>
+            <div className='rounded-lg px-2 shadow-lg bg-green-900'>
+              {user?.campus}
+            </div>
+            <div
+              className={classNames(
+                'rounded-lg px-2 shadow-lg',
+                {
+                  'bg-teal-700': user?.role === UserRole.ALUNO,
+                },
+                { 'bg-indigo-700': user?.role === UserRole.INTERFACE },
+                { 'bg-lime-700': user?.role === UserRole.ORIENTADOR }
+              )}
+            >
+              Aluno
+            </div>
+            <p className='truncate w-44'>{user !== undefined && user.name}</p>
+          </div>
         )}
       </div>
       <button
@@ -46,7 +62,9 @@ export default function UserInfo() {
           sessionStorage.removeItem('token');
           window.location.reload();
         }}
-        className={classNames('inline-block')}
+        className={classNames(
+          'inline-block whitespace-nowrap active:scale-90 hover:text-green-300'
+        )}
       >
         <FontAwesomeIcon icon={faSignOutAlt as IconProp} />
         &nbsp;Sair
